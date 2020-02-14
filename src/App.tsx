04c1826367caf8c49d28f9b7ec4428102a896593
Francesc Navarro <francesc.navarro@exoticca.com>
2020-02-14 11:06:44 +0100

    img responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import fnavarroGif from './img/fnavarro8bit.gif';
 import { Container } from "nes-react";
+import styled from 'styled-components/macro';
+
+const Img = styled.img`
+  max-width: 100%; 
+  display:block; 
+  height: auto;
+`;
 
 const App = () => {
   return (
     <div className="App">
       <Container>
-        <h1>Francesc Navarro Senior FrontEnd Engineer</h1>
-        <img src={fnavarroGif} alt="Francesc Navarro" />
+        <h1>Francesc Navarro</h1>
+        <h2>Senior FrontEnd Engineer</h2>
+        <Img src={fnavarroGif} alt="Francesc Navarro" />
       </Container>
     </div>
   );
