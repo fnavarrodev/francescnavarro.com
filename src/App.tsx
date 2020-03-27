d5b6407fe5b7a64409e4a476f5d8f37775a9c187
Francesc Navarro <francesc.navarro@exoticca.com>
2020-03-27 09:09:49 +0100

    Column/Row

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,65 @@ const Img = styled.img`
   height: auto;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SocialLinks = styled.div`
+  margin-top: 10px;
+`;
+
+const HeadingImage = styled.div`
+  display: flex;
+  @media (max-width: 768px) {
+    font-size: 9px;
+  }
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+`;
+
+const Column = styled.div`
+  flex-basis: 100%;
+  padding: 24px;
+  @media (min-width: 768px) {
+    flex: 1;
+  }
+`;
+
 const App = () => {
   return (
     <div className="App">
       <Container>
-        <h1>Francesc Navarro</h1>
-        <h2>Senior FrontEnd Engineer</h2>
-        <Img src={fnavarroGif} alt="Francesc Navarro" />
-        <br />
-        <a href={'https://twitter.com/bantidisturbis'}><Icon icon={'twitter'} is-large /></a>
-        &nbsp;
-        <a href={'https://github.com/fnavarrodev'}><Icon icon={'github'} is-large /></a>
-        &nbsp;
-        <a href={'https://www.linkedin.com/in/fnavarrodeveloper'}><Icon icon={'linkedin'} is-large /></a>
-        &nbsp;
+        <Wrapper>
+          <HeadingImage>
+            <Row>
+              <Column>
+                <Img src={fnavarroGif} alt="Francesc Navarro" />
+              </Column>
+              <Column>
+                <h1>Francesc Navarro</h1>
+                <br />
+                <h2>Senior FrontEnd Engineer</h2>
+              </Column>
+            </Row>
+          </HeadingImage>
+          <SocialLinks>
+            <a href={'https://twitter.com/bantidisturbis'}><Icon icon={'twitter'} is-large /></a>
+            &nbsp;
+            <a href={'https://github.com/fnavarrodev'}><Icon icon={'github'} is-large /></a>
+            &nbsp;
+            <a href={'https://www.linkedin.com/in/fnavarrodeveloper'}><Icon icon={'linkedin'} is-large /></a>
+            &nbsp;
+        </SocialLinks>
+        </Wrapper>
       </Container>
     </div>
   );
